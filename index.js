a513a58f90f5eec952f272f2a2cb55e0546455b9
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-18 22:55:33 +0600

    Rooms api added

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+const port = process.env.PORT || 8000;
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("study-nook");
+    const roomsCollection = db.collection("rooms");
+
+    app.get("/rooms", async (req, res) => {
+      const result = await roomsCollection.find().toArray();
+      res.json(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on: ${port}`);
+});
