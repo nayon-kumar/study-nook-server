98ecdcc89d22cab0eeb3200b9f3b1529d2da036b
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-19 21:46:26 +0600

    Edit room api added

--- index.js
+++ index.js
@@ -48,6 +48,19 @@ async function run() {
       res.json(result);
     });
 
+    // Edit room details
+    app.patch("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const result = await roomsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+      res.json(result);
+    });
+
+    // Delete room
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
